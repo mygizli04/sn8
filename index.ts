518e43aff9c5a20d1c5ca1ca7dc3ca05cfad157f
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-06-12 21:42:37 +0400

    Added sample commands
    Co-authored-by: DiscStrad <DiscStrad@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,44 @@
 import * as discord from 'discord.js'
 import buttons from 'discord-buttons'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const client = new discord.Client()
 buttons(client)
+
+client.on('ready', () => {
+	console.log("Holy smokes the bot is ready!")
+})
+
+client.on('message', message => {
+	
+	switch (message.content.toLowerCase()) {
+		case '&snek':
+			let snekButton = new buttons.MessageButton().setLabel("Delete").setStyle("red").setID('snek_button') // NO error
+			// @ts-expect-error
+			message.channel.send('Pizza', snekButton);
+		break
+		case '&clik':
+			let messageClick = new buttons.MessageButton().setLabel(" ").setStyle("blurple").setID('clik_button') // NO error
+			// @ts-expect-error
+			message.channel.send('Click to money', messageClick);
+		break
+	}
+})
+
+client.on('clickButton', async (button) => {
+	if (!button.deffered || !button.replied) {
+		switch (button.id) {
+			case "clik_button":
+                console.log(button.member)
+				await button.defer()
+			break;
+			case 'snek_button':
+				button.message.delete()
+			break
+		}
+	}
+});
+
+client.login(process.env.botToken)
 
