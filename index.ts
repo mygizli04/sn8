87806978d1dd69ed313f41f6b6f46fdeb3153e69
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2021-06-12 22:26:33 +0400

    Click command

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as discord from 'discord.js'
 import buttons from 'discord-buttons'
 import dotenv from 'dotenv'
+import mongo from 'mongodb'
 dotenv.config()
 
 const client = new discord.Client()
@@ -10,18 +11,44 @@ client.on('ready', () => {
 	console.log("Holy smokes the bot is ready!")
 })
 
-client.on('message', message => {
+
+let database: any;
+let userdata: any;
+let guilddata: any;
+
+const db = mongo.MongoClient.connect(process.env.dbURL as string, (err, dbClient) => {
+	if (!err) {
+		console.log("Connected to the mongoDB database!")
+		database = dbClient.db('sn8')
+		userdata = database.collection("userdata")
+		guilddata = database.collection("guilddata")
+	}
+	else {
+		console.error("Could not conenct to the database!")
+		process.exit(1)
+	}
+})
+
+interface userdata {
+	coins: number
+}
+
+client.on('message', async message => {
 	
 	switch (message.content.toLowerCase()) {
 		case '&snek':
 			// @ts-expect-error
-			let snekButton = new buttons.MessageButton().setLabel("Delete").setStyle("red").setID('snek_button') // NO error
+			let snekButton = new buttons.MessageButton().setLabel("Delete").setStyle("red").setID('snek_button')
 			message.channel.send('Pizza', snekButton);
 		break
 		case '&clik':
 			// @ts-expect-error
-			let messageClick = new buttons.MessageButton().setLabel(" ").setStyle("blurple").setID('clik_button') // NO error
-			message.channel.send('Click to money', messageClick);
+			message.channel.send('money' ,new buttons.MessageButton().setLabel("Click if ur kewl").setStyle("blurple").setID('clik_button'));
+		break
+		case '&coinz':
+			userdata.findOne({_id: message.member?.id}).then((data: userdata) => {
+				message.reply("you have " + data.coins + " coins.")
+			})
 		break
 	}
 })
@@ -31,7 +58,12 @@ client.on('clickButton', async (button) => {
 		switch (button.id) {
 			case "clik_button":
 				await button.clicker.fetch()
-				console.log(button.clicker.user)
+				let user = await userdata.findOne({_id: button.clicker.user.id})
+				if (user) {
+					await userdata.updateOne({_id: button.clicker.user.id}, { $set: {coins: user.coins + 1}})
+				} else {
+					await userdata.insertOne({_id: button.clicker.user.id, coins: 1})
+				}
 				await button.defer()
 			break;
 			case 'snek_button':
